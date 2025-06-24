5b34d28a97e7ddfbc8b9e7a87b5d2b4b4c931e9f
YMnooneJM <demraldo12@gmail.com>
2025-06-24 16:56:15 +0100

    feat: implement routing and create initial page components for the ecommerce app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 
+import { Outlet } from 'react-router-dom'
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <div>Ecommerce App</div>
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
   )
 }
 
