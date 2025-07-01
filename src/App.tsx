92f87e309b0acc7d93f190a0cf46c27f489b577c
oludoye olumide <oludoyeolumide@gmail.com>
2025-07-01 16:18:30 +0100

    feat: implement routing and create Login and Signup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <div>Ecommerce App</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
