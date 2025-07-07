02a0e19634e3bd25c1f49267d2f50f19e867a0d2
profsea112 <profsea112@gmail.com>
2025-07-07 11:39:19 +0100

    changes made

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,14 @@
 import Error404 from "../components/Error404"
-import Navbar from "../components/Navbar"
+import Footer from "../Components/Footer"
+import NavBar from "../components/NavBar"
+
 
 const NotFound = () => {
   return (
     <div>
-      <Navbar/>
+      <NavBar/>
       <Error404/>
+      <Footer/>
     </div>
   )
 }
