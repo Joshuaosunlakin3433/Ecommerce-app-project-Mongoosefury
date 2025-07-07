6bbd9c12717767352abcd1c66050615c4be99160
profsea112 <profsea112@gmail.com>
2025-07-07 10:05:59 +0100

    changes made

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,12 @@ import Home from "./pages/Home.tsx";
 import Contact from "./pages/Contact.tsx";
 import About from "./pages/About.tsx";
 
-import SignUp from "./pages/SignUp.tsx";
 import Login from "./pages/Login.tsx";
+import NotFound from "./pages/NotFound.tsx";
+
+import NewArrival from "./components/NewArrival.tsx";
+import SignUp from "./pages/Signup.tsx";
+
 
 // The index route is the default child route that will be rendered when the parent path is matched
 const router = createBrowserRouter([
@@ -18,8 +22,10 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "contact", element: <Contact /> },
       { path: "about", element: <About /> },
-      { path: "sign-up", element: <SignUp /> },
-      {path: "login", element: <Login/>}
+      { path: "Signup", element: <SignUp/>},
+      {path: "login", element: <Login/>},
+      {path: "NewArrival", element: <NewArrival />},
+      {path: "*", element: <NotFound/>}
     ],
   },
 ]);
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import Error404 from "../components/Error404"
+import Navbar from "../components/Navbar"
+
+const NotFound = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Error404/>
+    </div>
+  )
+}
+
+export default NotFound
